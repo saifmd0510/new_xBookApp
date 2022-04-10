121b54aa7a36953389ba18177fcddb91cfcfa76f
saifmd0510 <saifmd0510@gmail.com>
2022-04-10 07:26:57 -0300

    Text Change Test 2

--- lib/screens/view_seller_books/ViewSellerBooks.dart
+++ lib/screens/view_seller_books/ViewSellerBooks.dart
@@ -21,7 +21,7 @@ class _ViewSellerBooksState extends State<ViewSellerBooks> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text('All Books'),
+        title: Text('All Books to be sold'),
         ),
       body: StreamBuilder<QuerySnapshot>(
         stream: (title!= "" && title!= null ||  uid!="" && uid!=null)
